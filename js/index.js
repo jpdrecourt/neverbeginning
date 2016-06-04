6a02480ab5a618a78748d9b87266dc8c2cc43c95
Jean-Philippe Drecourt <jpdrecourt@gmail.com>
2016-06-04 10:01:26 +0100

    Changed the handling of the cursor.

--- js/index.js
+++ js/index.js
@@ -1,21 +1,20 @@
 var
-  cursor = "&#8248;",
+  $cursor = $("#cursor"),
   forgettingTime = 8000,
-  sIndex = 0, // Sound index to allow repetition.
-    // Sounds extracted from https://www.youtube.com/watch?v=hFWXgB0wpsU
+  // Sounds extracted from https://www.youtube.com/watch?v=hFWXgB0wpsU
   spaceSound = new buzz.sound("http://drecourt.com/wp-content/uploads/2016/05/space", {
     formats: ["ogg", "mp3"],
     preload: true,
     autoplay: false,
     loop: false
   }),
-    letterSound = new buzz.sound("http://drecourt.com/wp-content/uploads/2016/05/letter", {
+  letterSound = new buzz.sound("http://drecourt.com/wp-content/uploads/2016/05/letter", {
     formats: ["ogg", "mp3"],
     preload: true,
     autoplay: false,
     loop: false
   }),
-    crSound = new buzz.sound("http://drecourt.com/wp-content/uploads/2016/05/carriage-return", {
+  crSound = new buzz.sound("http://drecourt.com/wp-content/uploads/2016/05/carriage-return", {
     formats: ["ogg", "mp3"],
     preload: true,
     autoplay: false,
@@ -23,23 +22,23 @@ var
   });
 
 function appendHtml(htmlText) {
-  var t = $(".text").last().html();
-  if (htmlText == " " && t.slice(-2, -1) == " ") {
+  var $t = $(".line:last > .text");
+  if (htmlText == " " && $t.html().slice(-1) == " ") {
     // Allows for multiple spaces
-    $(".text").last().html(t.slice(0, -2) + "&nbsp; " + cursor);
+    $t.html($t.html().slice(0, -1) + "&nbsp; ");
   } else {
-    $(".text").last().html(t.slice(0, -1) + htmlText + cursor);
+    $t.html($t.html() + htmlText);
   }
 }
 
 function lineCount() {
-  return $("div.text").not(".forgotten").length;
+  return $("div.line").not(".forgotten").length;
 }
 
 // Insert a line break if necessary
 function checkForLineBreak() {
-  var t = $(".text").last();
-  if (t.css("height") > t.css("line-height")) {
+  var $l = $(".line:last");
+  if ($l.css("height") > $l.css("line-height")) {
     insertLineBreak();
   }
 }
@@ -47,20 +46,23 @@ function checkForLineBreak() {
 // Insert a line break to know where to delete the line
 function insertLineBreak() {
   crSound.stop().play();
-  var t = $('.text').last().html();
-  var s = t.lastIndexOf(" ");
-  $('.text').last().html(t.slice(0, s));
-  $('.textWrapper').append("<div class='text'>" + t.slice(s + 1) + "</div>");
+  var $l = $('.line:last'),
+      $t = $('.line:last > .text'),
+      c = $('#cursor')[0].outerHTML,
+      s = $t.html().lastIndexOf(" ");
+  $t.html($t.html().slice(0, s));
+  $l.html($t);
+  $('.textWrapper').append("<div class='line'><span class='text'>" + $t.html().slice(s + 1) + "</span>" + c + "</div>");
   //  t.html(t.slice(0, s) + breakMarker + t.slice(s + 1));
 }
 
-// Refine the disappearance 
+// Refine the disappearance
 // Use something proportional to the length of the text
 // Start the opacity as soon as one or two lines are written and then slide up slowly as the text becomes bigger.
 // Maybe a random fullscreen effect to distract the viewer?
 function forgetStory() {
   if (lineCount() > 2) {
-    var t = $(".text").not(".forgotten").first();
+    var t = $(".line").not(".forgotten").first();
     var marginText = "-" + t.css("height");
     t.addClass("forgotten")
       .animate({
@@ -83,7 +85,7 @@ function forgetStory() {
 $(document).keydown(function(event) {
   switch (event.which) {
     case 8:
-      // Prevent using backspace 
+      // Prevent using backspace
       event.preventDefault();
       break;
     case 9:
@@ -95,7 +97,7 @@ $(document).keydown(function(event) {
     case 13:
       // Add new line with enter
       event.preventDefault();
-      if ($(".text").last().text().length == 1) {
+      if ($(".line:last > .text").text().length == 0) {
         // Makes sure an empty line appears on screen.
         appendHtml("&nbsp; ");
       } else {
@@ -106,7 +108,7 @@ $(document).keydown(function(event) {
     case 32:
       event.preventDefault();
       spaceSound.stop().play();
-      appendHtml(" ");      
+      appendHtml(" ");
     default:
       // Nothing special happening but the sound
   }
@@ -121,11 +123,7 @@ $(window).keypress(function(event) {
   forgetStory();
 });
 
-// Insert the cursor
 $(document).ready(function() {
-  // Insert the cursor
-  $(".text").html(cursor);
-
   // Prevent selection (from http://www.rgagnon.com/jsdetails/js-0120.html)
   if (typeof document.onselectstart != "undefined") {
     document.onselectstart = new Function("return false");
